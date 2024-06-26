d789cf01b4a5fe39b121676a1a4489edd5da00f9
devmobi1812 <144266743+devmobi1812@users.noreply.github.com>
2024-06-26 19:18:56 -0300

    Optimizacion de JS y unificacion de HTML
    Se unifico el html, y se opto por la version de la ventana flotante para mostrar la descripcion de las materias.
    Se optimizo  el JS.
    
    Co-Authored-By: Dario213987 <100569837+Dario213987@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -1,85 +1,27 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function(){
-    let botones = [
-        "btn-algoritmos",
-        "btn-arquitectura",
-        "btn-sis-organizaciones",
-        "btn-cs-tec-sociedad",
-        "btn-practicas-profesionalizantes1",
-        "btn-algebra",
-        "btn-matematica1",
-        "btn-ingles1",
-        "btn-algoritmos2",
-        "btn-sistop",
-        "btn-basedatos",
-        "btn-ing-software1",
-        "btn-practicas-profesionalizantes2",
-        "btn-estadistica",
-        "btn-matematica2",
-        "btn-ingles2",
-        "btn-algoritmos3",
-        "btn-redes-comunic",
-        "btn-sem-actualizacion",
-        "btn-ing-software2",
-        "btn-practicas-profesionalizantes3",
-        "btn-ingles3",
-        "btn-asp-legales"
-    ];
-
-    let ventanas = [
-        "alg1",
-        "arquitectura",
-        "syo",
-        "ctys",
-        "pp1",
-        "algebra",
-        "am1",
-        "ing1",
-        "alg2",
-        "so",
-        "bd",
-        "ingsoft1",
-        "pp2",
-        "estadistica",
-        "am2",
-        "ing2",
-        "alg3",
-        "ryc",
-        "sda",
-        "ingsoft2",
-        "pp3",
-        "ing3",
-        "alp"
-    ];
-
-    for (let i = 0; i < botones.length; i++) {
-        document.getElementById(botones[i]).addEventListener("click", function() {
-            mostrarDescripcion(i);
-        });
-    }
-
-    for (let i = 0; i < botones.length; i++) {
-        document.getElementById(botones[i]).addEventListener("click", function() {
-            mostrarDescripcion(i);
-        });
-    }
-
+    let materias = document.querySelectorAll(".anio > button")
+    
+    materias.forEach(materia => materia.addEventListener("click", function(e){
+        let nombreMateria = e.currentTarget.id.replace("btn-","");
+        console.log(nombreMateria)
+        mostrarDescripcion(nombreMateria);
+    }))
 
     document.querySelector(".cerrar").addEventListener("click", cerrarVentanas);
 
-    function mostrarDescripcion(indice) {
-        let ventana = document.getElementById(ventanas[indice]);
+    function mostrarDescripcion(nombreMateria) {
+        let ventana = document.getElementById(nombreMateria);
         ventana.classList.remove("invisible");
         ventana.classList.add("ventanas-flotantes");
     }
 
     function cerrarVentanas() {
-        for (let i = 0; i < ventanas.length; i++) {
-            const ventana = document.getElementById(ventanas[i]);
-            ventana.classList.remove("ventanas-flotantes");
-            ventana.classList.add("invisible");
-        }
+        let ventana = document.querySelector(".ventanas-flotantes");
+        ventana.classList.remove("ventanas-flotantes");
+        ventana.classList.add("invisible");
+        
     }
     
 })
